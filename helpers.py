bd6335430a90311caa5b59571b865c981dd62171
berinaspirjan <berina.spirjan@treca-gimnazija.edu.ba>
2023-12-30 00:49:14 +0100

    Added logic for recognizing brain cancer

--- helpers.py
+++ helpers.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+
+from keras.preprocessing.image import ImageDataGenerator
 plt.style.use('ggplot')
 
 
@@ -20,3 +22,29 @@ def plot_from_image_path(number_of_rows, number_of_columns, list_of_image_paths,
         plt.imshow(image)
         plt.imshow(mask, alpha=0.5)
     plt.show()
+
+
+def dice_coefficients(y_true, y_pred, smooth=100):
+    y_true_flatten = K.flatten(y_true)
+    y_pred_flatten = K.flatten(y_pred)
+
+    intersection = K.sum(y_true_flatten * y_pred_flatten)
+    union = K.sum(y_true_flatten) + K.sum(y_pred_flatten)
+    return (2 * intersection + smooth) / (union + smooth)
+
+
+def dice_coefficients_loss(y_true, y_pred, smooth=100):
+    return -dice_coefficients(y_true, y_pred, smooth)
+
+
+def iou(y_true, y_pred, smooth=100):
+    intersection = K.sum(y_true * y_pred)
+    sum = K.sum(y_true + y_pred)
+    iou = (intersection + smooth) / (sum - intersection + smooth)
+    return iou
+
+
+def jaccard_distance(y_true, y_pred):
+    y_true_flatten = K.flatten(y_true)
+    y_pred_flatten = K.flatten(y_pred)
+    return -iou(y_true_flatten, y_pred_flatten)
